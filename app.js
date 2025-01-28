2d8820631c86d81a54925469de1011d050028bf1
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2025-01-28 20:42:23 +0100

    ajout du middleware pour gérer les fichiers css

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const bodyParser = require('body-parser');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//middleware to handle css properly
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes);
 
