0e7ad7aca95ade671bbc73ea072a45cd1e86ea2e
JULIEN006 <146907880+JULIEN006@users.noreply.github.com>
2025-01-30 11:33:06 +0100

    correction user.js

--- models/User.js
+++ models/User.js
@@ -1,20 +1,19 @@
 const mongoose = require('mongoose');
 
+
 const userSchema = new mongoose.Schema({
-    datasetid: String,
-    recordid: String,
-    firstname: String,
-    lastname: String,
     email: {
         type: String,
+        required: true,
         unique: true,
-        required: true
+        trim: true,
+        lowercase: true
     },
     password: String,
     roles: [String],
     borrowedDocuments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Document' }]
 });
 
-userSchema.index({ 'fields.email': 1 }, { unique: true });
+userSchema.index({ email: 1 }, { unique: true });
 
 module.exports = mongoose.model('User', userSchema);
