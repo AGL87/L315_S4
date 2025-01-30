7baee495830980d0995fa39c3f70e553cc8eaeed
JULIEN006 <146907880+JULIEN006@users.noreply.github.com>
2025-01-30 11:54:47 +0100

    rejout

--- models/User.js
+++ models/User.js
@@ -1,18 +1,21 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true
-    },
-    password: String,
-    roles: [String],
-    borrowedDocuments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Document' }]
-});
-
-userSchema.index({ email: 1 }, { unique: true });
-
-module.exports = mongoose.model('User', userSchema);
+    datasetid: String,
+    recordid: String,
+    firstname: String,
+    lastname: String,
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        password: String,
+        roles: [String],
+        borrowedDocuments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Document' }]
+    });
+    
+    userSchema.index({ email: 1 }, { unique: true });
+    
+    module.exports = mongoose.model('User', userSchema);
